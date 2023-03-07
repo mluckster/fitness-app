d9e61ea55417390b68cf8cf4c9170b2144e712fa
Michael Lucky <luckymichael00@gmail.com>
2023-03-06 23:48:09 -0500

    edit/delete exercises

--- fitness-app/controllers/exercises.js
+++ fitness-app/controllers/exercises.js
@@ -3,6 +3,9 @@ var Workout = require('../models/workout')
 module.exports = {
     new: newExercise,
     create,
+    delete: deleteExer,
+    edit,
+    update,
 }
 
 function newExercise(req, res) {
@@ -16,5 +19,53 @@ function create(req, res) {
         workout.exercise.push(req.body)
         workout.save()
         .then(() => res.redirect(`/workouts/${req.params.id}`))
+    })
+}
+
+// Finds index of workout.exercise where the ID is the same from the 
+// button click, deletes the item in the arry and saves
+function deleteExer (req, res) {
+    console.log(req.params)
+    Workout.findById(req.params.wid)
+    .then(workout => {
+        var idx = workout.exercise.findIndex((exer) => {
+            if(JSON.stringify(exer._id)===`"${req.params.eid}"`){
+                return true
+            } 
+        })
+        workout.exercise.splice(idx, 1)
+        workout.save()
+        .then(() => res.redirect(`/workouts/${req.params.wid}`))
+    })
+}
+
+function edit(req, res) {
+    var eid = req.params.eid
+    var wid = req.params.wid
+    Workout.findById(req.params.wid)
+    .then(workout => {
+        var idx = workout.exercise.findIndex((exer) => {
+            if(JSON.stringify(exer._id)===`"${req.params.eid}"`){
+                return true
+            } 
+        })
+    var exercise = workout.exercise[idx]
+    res.render('exercises/edit', { title: "Edit Exercise Info", exercise, eid, wid } )
+    })
+}
+
+function update(req, res) {
+    console.log(req.params)
+    console.log(req.body)
+    Workout.findById(req.params.wid)
+    .then(workout => {
+        var idx = workout.exercise.findIndex((exer) => {
+            if(JSON.stringify(exer._id)===`"${req.params.eid}"`){
+                return true
+            } 
+        })
+        workout.exercise[idx]=req.body
+        workout.save()
+        .then(() => res.redirect(`/workouts/${req.params.wid}`))
     })
 }
--- fitness-app/routes/exercises.js
+++ fitness-app/routes/exercises.js
@@ -5,5 +5,8 @@ var exercisesCtrl = require('../controllers/exercises')
 /* GET users listing. */
 router.get('/workouts/:id/exercises/new', exercisesCtrl.new)
 router.post('/workouts/:id/exercises/', exercisesCtrl.create)
+router.delete('/workouts/:wid/exercises/:eid', exercisesCtrl.delete)
+router.get('/workouts/:wid/exercises/:eid/edit', exercisesCtrl.edit)
+router.put('/workouts/:wid/exercises/:eid', exercisesCtrl.update)
 
 module.exports = router;
